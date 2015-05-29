b2bd32fcf66bc0a691b1a4509176ea977748feae
Devin Boyer <drb5272@gmail.com>
2015-05-29 09:37:17 -0400

    update message

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import cherrypy
 
 class HelloWorld(object):
   def index(self):
-    return "Hello World"
+    return "Hello World (version 2.0!)"
   index.exposed = True
 
 cherrypy.quickstart(HelloWorld())
